92f6f542e33c03a5dc94f7ce2cdf885af2aa7b2f
David García Garzón <david.garcia@somenergia.coop>
2023-12-29 19:49:33 +0100

    ♻️ Loading: parametrization, simplification and fixes
    - Fix: the ball and the trail get dealigned in some cases
    - Fix: The ball and the trail get desyncronized in som cases
    - Now both are fixed childs of a rotating parent.
    - All the pixel adjustment are now computed on parameters
    - All the childs are concentric and absolute positioned
    - Extracted parameters as attributes (width, color, ballsize,
      trailwidth...)

--- lib/Loading/index.jsx
+++ lib/Loading/index.jsx
@@ -1,96 +1,101 @@
 import Logo from '../../src/assets/logo.svg'
+import PropTypes from 'prop-types'
 import Box from '@mui/material/Box'
+import { keyframes } from '@mui/system'
 
-export default function Loading() {
+export default function Loading({
+  color = '#96b633',
+  width = '150px',
+  trailWidth = '3px',
+  ballWidth = '16px',
+  logoCoverage = '78%',
+  logoOffset = '-12%',
+  sx,
+}) {
+  const electronSpin = keyframes`
+    to { rotate: 1turn; }
+  `
+
   return (
     <Box
       sx={{
+        boxSizing: 'content-box',
+        position: 'relative',
         width: '100%',
         minHeight: '80%',
         display: 'flex',
         flexFlow: 'row wrap',
         justifyContent: 'center',
         alignContent: 'center',
-        gap: '10%',
-        p: 3,
+        ...sx,
       }}
     >
       <Box
         sx={{
-          position: 'inherit',
-          transform: 'translate(0%, 0%)',
-          width: '150px',
-          height: '150px',
+          position: 'relative',
+          width: width,
+          height: width,
           borderRadius: '50%',
           textAlign: 'center',
-          lineHeight: '150px',
           background: 'transparent',
-          color: '#96b633',
+          color: `${color}`,
           boxShadow: '0 0 20px rgba(0, 0, 0, 0.2)',
-          '&:before': {
-            content: '""',
-            position: 'absolute',
-            top: '-0px',
-            left: '-0px',
-            width: '100%',
-            height: '100%',
-            border: '3px solid transparent',
-            borderTopColor: '#96b633',
-            borderRightColor: '#96b633',
-            borderRadius: '50%',
-            animation: 'animateDot 2s linear infinite',
-          },
-          '@keyframes animateDot': {
-            '0%': {
-              transform: 'rotate(0deg)',
-            },
-            '100%': {
-              transform: 'rotate(360deg)',
-            },
-          },
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        <img
-          src={Logo}
-          style={{
-            maxHeight: '7rem',
-            marginTop: '0.5rem',
-          }}
-        />
         <Box
           sx={{
-            display: 'block',
             position: 'absolute',
-            top: 'calc(50% - 2px)',
-            left: '50%',
-            width: '50%',
-            height: '4px',
-            background: 'transparent',
-            transformOrigin: 'left',
-            animation: 'animateLine 2s linear infinite',
-
+            animation: `${electronSpin} 2s linear infinite`,
+            width: '100%',
+            height: '100%',
             '&:before': {
               content: '""',
               position: 'absolute',
-              width: '16px',
-              height: '16px',
-              top: '-6px',
-              right: '-8px',
+              top: `-${trailWidth}`,
+              left: `-${trailWidth}`,
+              width: '100%',
+              height: '100%',
               borderRadius: '50%',
-              background: '#96b633',
-              boxShadow: '0 0 20px #96b633',
+              border: `${trailWidth} solid transparent`,
+              borderTopColor: color,
+              borderRightColor: color,
+              rotate: '-45deg',
             },
-            '@keyframes animateLine': {
-              '0%': {
-                transform: 'rotate(45deg)',
-              },
-              '100%': {
-                transform: 'rotate(405deg)',
-              },
+            '&:after': {
+              content: '""',
+              position: 'absolute',
+              width: ballWidth,
+              height: ballWidth,
+              top: `calc(50% - ${ballWidth} / 2)`,
+              right: `calc((-${ballWidth} - ${trailWidth}) / 2)`,
+              borderRadius: '50%',
+              backgroundColor: color,
+              boxShadow: `0 0 20px`,
+              boxShadowColor: color,
             },
           }}
         ></Box>
+        <img
+          src={Logo}
+          style={{
+            marginTop: logoOffset,
+            maxWidth: logoCoverage,
+            maxHeight: logoCoverage,
+          }}
+        />
       </Box>
     </Box>
   )
+}
+Loading.propTypes = {
+  color: PropTypes.string,
+  width: PropTypes.string,
+  trailWidth: PropTypes.string,
+  ballWidth: PropTypes.string,
+  logoCoverage: PropTypes.string,
+  logoOffset: PropTypes.string,
+  sx: PropTypes.object,
 }
