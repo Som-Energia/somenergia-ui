daea31d251943715d351f04020fa4a88d29df296
David García Garzón <david.garcia@somenergia.coop>
2023-12-30 19:55:16 +0100

    🏗️ fixes to remove module duplication on the app
    - No dependencies, just peer and dev dependencies
    - properly mark peerdependencies as external
    - Added module format infix to the output files
    - Split vendor chunks (they should not appear but this way we'll notice)

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteyaml from '@modyfi/vite-plugin-yaml'
 import svgr from 'vite-plugin-svgr'
-import { join, resolve, relative, extname } from 'path'
+import { isAbsolute, join, resolve, relative, extname } from 'path'
 import { glob } from 'glob'
+import fs from 'fs'
 import { fileURLToPath } from 'node:url'
 import pkg from './package.json'
+
+const isExternal = (id) => !id.startsWith('.') && !isAbsolute(id)
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,11 +22,13 @@ export default defineConfig({
     rollupOptions: {
       // Consider all dependencies external (do not bundle them)
       external: [
-        // Should match all direct imports to 3rd party modules
         /^react/,
+        'react-i18next',
+        'i18next',
+        'i18next-browser-languagedetector',
+        'prop-types',
         /^@mui/,
-        /^i18next/,
-        /prop-types/,
+        /node_modules/,
       ],
       // create modlue A/B/module from lib/A/B/module.jsx
       input: Object.fromEntries(
@@ -37,8 +42,12 @@ export default defineConfig({
       output: {
         // Enable preserveModules to check any 3rd party being bundled
         //preserveModules: true,
-        entryFileNames: '[name].js',
+        entryFileNames: '[name].[format].js',
         assetFileNames: 'assets/[name]-[hash][extname]',
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) return 'vendor'
+          //return id.toString().split('node_modules/')[1].split('/')[0].toString()
+        },
       },
     },
   },
