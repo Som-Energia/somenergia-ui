32d63b50f26ea144d1a30493ee85031064883fa2
dukenuken09 <gilgassorovira@gmail.com>
2024-01-25 12:59:43 +0100

    ✨ Add the incredible chart

--- lib/Chart/index.jsx
+++ lib/Chart/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import PropTypes from 'prop-types'
 
 import {
   Bar,
@@ -9,16 +9,69 @@ import {
   YAxis,
   Label,
   Legend,
+  LineChart,
   BarChart,
+  Line
 } from 'recharts'
 
-import { formatTooltipLabel, formatXAxis } from 'services/utils'
+import { formatTooltipLabel, formatXAxis, ticksFromData, domainFromData, formatDecimal, formatTooltip, mergeData} from './dataFormat'
 
-function Chart({ data, period, legend = false }) {
-
-  return (
-    <div style={{ height: '450px' }}>
-      <ResponsiveContainer>
+function Chart({ data, period, legend = false, compareData, type }) {
+  
+  const getChartType = (type, data, period, legend, compareData) => {
+    if (type === 'LINE') {
+      const mixedData = mergeData(data, compareData)
+      return (
+        <LineChart width={730} height={250} data={mixedData}>
+          <CartesianGrid stroke="#ccc" strokeWidth={0.5} vertical={false} />
+          <XAxis
+            dataKey="date"
+            type="number"
+            ticks={ticksFromData(mixedData, period)}
+            scale="time"
+            domain={domainFromData(mixedData, period)}
+            tickFormatter={(tickItem) => formatXAxis(period, tickItem)}
+            padding={{ left: 24, right: 24 }}
+            tick={{ fontSize: '1rem', transform: 'translate(0, 8)' }}
+          />
+          <YAxis
+            type="number"
+            domain={[0, 'auto']}
+            axisLine={false}
+            tickCount={8}
+            tickLine={false}
+            tickFormatter={(tickItem) => `${formatDecimal(tickItem / 1000)}`}
+            tick={{ fontSize: '1rem', transform: 'translate(0, 0)' }}
+          >
+            <Label value="kWh" angle={-90} position="insideLeft" fill="#969696" />
+          </YAxis>
+          <Tooltip
+            formatter={formatTooltip}
+            labelFormatter={(value) => formatTooltipLabel(period, value, 'lineChart')}
+            contentStyle={{ fontWeight: 'bold' }}
+          />
+          <Line
+            type="monotone"
+            dataKey="value"
+            stroke="#96b633"
+            dot={false}
+            strokeWidth={3}
+            activeDot={{ r: 6 }}
+          />
+          {compareData && (
+            <Line
+              type="monotone"
+              dataKey="compValue"
+              stroke="#f2970f"
+              dot={false}
+              strokeWidth={3}
+              activeDot={{ r: 6 }}
+            />
+          )}
+        </LineChart>
+      )
+    } else {
+      return (
         <BarChart width={730} height={250} data={data.periods}>
           <CartesianGrid stroke="#616161" strokeWidth={0.5} vertical={false} />
           <XAxis
@@ -36,12 +89,7 @@ function Chart({ data, period, legend = false }) {
             tickLine={false}
             tick={{ fontSize: '1rem', transform: 'translate(0, 0)' }}
           >
-            <Label
-              value="kWh"
-              angle={-90}
-              position="insideLeft"
-              fill="#969696"
-            />
+            <Label value="kWh" angle={-90} position="insideLeft" fill="#969696" />
           </YAxis>
 
           <Tooltip
@@ -61,9 +109,25 @@ function Chart({ data, period, legend = false }) {
             )
           })}
         </BarChart>
+      )
+    }
+  }
+
+  return (
+    <div style={{ height: '450px' }}>
+      <ResponsiveContainer>
+        {getChartType(type, data, period, legend, compareData)}
       </ResponsiveContainer>
     </div>
   )
+}
+
+Chart.propTypes = {
+  data: PropTypes.array || PropTypes.object,
+  period: PropTypes.string,
+  legend: PropTypes.boolean || PropTypes.any,
+  compareData: PropTypes.array || null,
+  type: PropTypes.string,
 }
 
 export default Chart
