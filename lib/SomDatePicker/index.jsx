fbdc1a2cb2480a09296be98f37da92d7fde9ca4c
joana <joana.figueira@somenergia.coop>
2024-03-14 16:20:46 +0100

    ✨ Som datePicker
    Co-authored-by: Marta <marta.fernandez@somenergia.coop>

---
+++ lib/SomDatePicker/index.jsx
@@ -0,0 +1,71 @@
+import PropTypes from 'prop-types'
+import Box from '@mui/material/Box'
+import { Button } from '@mui/material'
+import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined'
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import dayjs from 'dayjs'
+import { useState } from 'react'
+import minMax from 'dayjs/plugin/minMax'
+
+dayjs.extend(minMax)
+
+export default function SomDatePicker({
+  color = '#96b633',
+  firstDate = dayjs().subtract(7, 'day'),
+  lastDate = dayjs().add(1, 'day'),
+  period = 'DAILY',
+  sx,
+}) {
+  const dayjsperiods = {
+    DAILY: 'd',
+    WEEKLY: 'w',
+    MONTHLY: 'M',
+    YEARLY: 'y',
+  }
+  const [currentTime, setCurrentTime] = useState(dayjs(firstDate))
+
+  function prevTimeWindow() {
+    setCurrentTime(
+      dayjs.max(dayjs(firstDate), currentTime.subtract(1, dayjsperiods[period])),
+    )
+  }
+
+  function nextTimeWindow() {
+    setCurrentTime(dayjs.min(dayjs(lastDate), currentTime.add(1, dayjsperiods[period])))
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        ...sx,
+      }}
+    >
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Button onClick={prevTimeWindow}>
+          <ArrowBackIosOutlinedIcon sx={{ color: color }} />
+        </Button>
+        <DatePicker
+          value={currentTime}
+          onChange={setCurrentTime}
+          minDate={dayjs(firstDate)}
+          maxDate={dayjs(lastDate)}
+          format="DD/MM/YYYY"
+        ></DatePicker>
+        <Button onClick={nextTimeWindow}>
+          <ArrowForwardIosOutlinedIcon sx={{ color: color }} />
+        </Button>
+      </LocalizationProvider>
+    </Box>
+  )
+}
+SomDatePicker.propTypes = {
+  color: PropTypes.string,
+  firstDate: dayjs,
+  lastDate: dayjs,
+  period: PropTypes.string,
+  sx: PropTypes.object,
+}
