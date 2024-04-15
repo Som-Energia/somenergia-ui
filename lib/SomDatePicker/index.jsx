f6c8f01abe28ff1c3aa91093c43e3cff4041302d
David García Garzón <david.garcia@somenergia.coop>
2024-04-15 15:12:53 +0200

    🧹  Remove unused import
    Co-authored-by: Joana Figueira <joana.figueira@somenergia.coop>

--- lib/SomDatePicker/index.jsx
+++ lib/SomDatePicker/index.jsx
@@ -6,7 +6,7 @@ import Button from '@mui/material/Button'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import dayjs, { Dayjs } from 'dayjs'
+import dayjs  from 'dayjs'
 import minMax from 'dayjs/plugin/minMax'
 import { i18n } from '../i18n'
 
