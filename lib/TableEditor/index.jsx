19adcd967f20e7229b592c7b6453420add7e50b6
David García Garzón <david.garcia@somenergia.coop>
2024-05-23 21:27:48 +0200

    🎨 TableEditor: simplified selection updates

--- lib/TableEditor/index.jsx
+++ lib/TableEditor/index.jsx
@@ -396,22 +396,9 @@ function TableEditor(props) {
       return
     }
     setSelected((selected) => {
-      const selectedIndex = selected.indexOf(id)
-      let newSelected = []
-
-      if (selectedIndex === -1) {
-        newSelected = newSelected.concat(selected, id)
-      } else if (selectedIndex === 0) {
-        newSelected = newSelected.concat(selected.slice(1))
-      } else if (selectedIndex === selected.length - 1) {
-        newSelected = newSelected.concat(selected.slice(0, -1))
-      } else if (selectedIndex > 0) {
-        newSelected = newSelected.concat(
-          selected.slice(0, selectedIndex),
-          selected.slice(selectedIndex + 1),
-        )
-      }
-      return newSelected
+      const wasSelected = selected.indexOf(id) !== -1
+      if (!wasSelected) return [...selected, id]
+      return selected.filter((x)=>x!==id)
     })
   }, [])
 
