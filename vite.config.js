979e8b4e2f8febebec29dc4791f2dab5966ad00b
Mario <mario.martinez@somenergia.coop>
2026-02-18 14:47:42 +0100

    🦖 ♻️ export default ConsumptionDisplay component and up version

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteyaml from '@modyfi/vite-plugin-yaml'
 import svgr from 'vite-plugin-svgr'
-import { isAbsolute, join, resolve, relative, extname } from 'path'
+import { resolve, relative, extname } from 'path'
 import { glob } from 'glob'
-import fs from 'fs'
 import { fileURLToPath } from 'node:url'
-import pkg from './package.json'
-
-const isExternal = (id) => !id.startsWith('.') && !isAbsolute(id)
 
 // https://vitejs.dev/config/
 export default defineConfig({
