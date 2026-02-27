245af1d1a3c2aa525a7f0d60910c57c05e39bfdf
Marite Guerrieri <marite.guerrieri@somenergia.coop>
2026-02-27 15:56:03 +0100

    ♻ sx instead styled in SummaryPrice

--- lib/SummaryPrice/SummaryPrice.jsx
+++ lib/SummaryPrice/SummaryPrice.jsx
@@ -1,49 +1,58 @@
-import styled from 'styled-components'
-
-const Wrapper = styled.div`
-  padding: 2px 4px;
-  color: '#4D4D4D';
-  align-items: center;
-  text-align: center;
-  flex: 1;
-`
-const Value = styled.div`
-  font-size: 1.5rem;
-  font-weight: 400;
-  padding: 0 4px 0 8px;
-  white-space: nowrap;
-  display: flex;
-  flex-direction: column;
-  .units {
-    font-size: 0.8rem;
-  }
-`
-const Detail = styled.div`
-  font-size: 0.7rem;
-  font-weight: 700;
-  padding: 4px 4px 0 8px;
-  line-height: 1.2rem;
-  .title {
-    color: #616161;
-  }
-  div {
-    white-space: nowrap;
-  }
-`
+import { Box, Typography } from '@mui/material'
 
 function SummaryPrice(props) {
   const { value, unit, description } = props
-  return (
-    <Wrapper>
-      <Value>
-        <span>{value}</span>
-        <span className="units">{unit}</span>
-      </Value>
-      <Detail>
-        <div>{description}</div>
-      </Detail>
-    </Wrapper>
-  )
+  return (<>
+    <Box
+      sx= {{
+        padding: '2px 4px',
+        alignItems: 'center',
+        textAlign: 'center',
+        flex: '1',
+      }}
+    >
+      <Box
+        sx = {{
+          padding: '0 4px 0 8px',
+          display: 'flex',
+          flexDirection: 'column',
+          whiteSpace: 'nowrap',
+        }}
+      >
+        <Typography
+          sx = {{
+            fontSize: '1.5rem',
+            fontWeight: '400',
+          }}
+        >
+            {value}
+        </Typography>
+        <Typography
+          sx = {{
+            fontWeight: '400',
+            fontSize: '0.8rem'
+          }}
+        >
+          {unit}
+        </Typography>
+      </Box>
+      <Box
+        sx = {{
+          padding: '4px 4px 0 8px',
+        }}
+      >
+        <Typography
+          sx={{
+            lineHeight: '1.2rem',
+            fontWeight: '700',
+            fontSize: '0.7rem',
+            whiteSpace: 'nowrap'
+          }
+        }>
+          {description}</Typography>
+      </Box>
+    </Box>
+  </>)
 }
 
 export default SummaryPrice
