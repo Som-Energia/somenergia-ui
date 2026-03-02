a2f0433bd7bc0584dc82812eb8ac767486b1b5e0
joana <joana.figueira@somenergia.coop>
2026-03-02 13:21:51 +0100

    📝 Add documentation
    Co-authored-by: Marite Guerrieri <marite.guerrieri@somenergia.coop>

--- lib/SomDatePicker/stories.jsx
+++ lib/SomDatePicker/stories.jsx
@@ -5,13 +5,24 @@ export default {
   title: 'Base Components/SomDatePicker',
   component: SomDatePickerExample,
   parameters: {
-    layout: 'centered',
+    layout: 'centered'
   },
   tags: ['autodocs'],
   argTypes: {
+    firstDate: {
+      control: false,
+      description: 'Minimal date allowed, if `null` no initial limit is set in the calendar'
+    },
+    lastDate: {
+      control: false,
+      description: 'Maximum date allowed, if `null` no final limit is set in the calendar'
+    },
     period: {
       control: { type: 'select' },
       options: ['DAILY', 'WEEKLY', 'MONTHLY', 'YEARLY'],
+    },
+    styles: {
+      description: 'Allow define `container` and `datepicker` styles'
     },
     currentTime: { type: 'date' },
     toolbarTitle: { type: 'string' },
@@ -23,6 +34,27 @@ export default {
 
 export const YearlyWithoutPrevNextButon = {
   args: {
+    period: 'YEARLY',
+    styles: {
+      datePicker: {
+        borderColor: 'secondary.main',
+        '& .MuiOutlinedInput-root': { borderRadius: '8px' },
+        input: {
+          textAlign: 'center',
+        },
+      }
+    },
+    toolbarTitle: 'Select date',
+    currentTime: dayjs(),
+    prevNextButtons: true,
+    disabledNextPrev: false,
+  }
+}
+
+export const YearlyWithFirstLastDate = {
+  args: {
+    firstDate: dayjs().year(2024),
+    lastDate: dayjs().year(2028),
     period: 'YEARLY',
     styles: {
       datePicker: {
